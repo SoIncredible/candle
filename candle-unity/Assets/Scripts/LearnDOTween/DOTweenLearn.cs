32b3cf4dd3ecdc12e669721cddccd9c965418d9b
EddieLee <263060652@qq.com>
2024-01-15 23:33:05 +0800

    [feat]: 导入DoTween

---
+++ candle-unity/Assets/Scripts/LearnDOTween/DOTweenLearn.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LearnDOTween
+{
+    public class DOTweenLearn : MonoBehaviour
+    {
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private Button clickBtn;
+
+        private Vector3 _originPos;
+
+        private void Awake()
+        {
+            _originPos = rectTransform.localPosition;
+            clickBtn.onClick.AddListener(OnClickBtn);
+        }
+
+
+        private void OnClickBtn()
+        {
+            var nowPosY = _originPos.y + rectTransform.rect.size.y;
+            rectTransform.localPosition = new Vector3(_originPos.x, nowPosY, _originPos.z);
+            rectTransform.DOLocalMove(_originPos, 0.3f).SetEase(Ease.OutQuad);
+        }
+    }
+}
---
+++ candle-unity/Assets/Scripts/LearnUniTask/UniTaskInUnityFunc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.LowLevel;
+
+namespace LearnUniTask
+{
+    public class UniTaskInUnityFunc : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+
+        private void InjectFunction()
+        {
+            PlayerLoopSystem playerLoopSystem = PlayerLoop.GetCurrentPlayerLoop();
+        }
+
+        private void OnClickDelaySecond()
+        {
+        }
+
+        private void OnClickDelayFrame()
+        {
+        }
+    }
+}
