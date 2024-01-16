e800fb9d7c786e606582c09dcc81c8819736c3f5
EddieLee <263060652@qq.com>
2024-01-17 01:01:02 +0800

    [feat]: 替换字体 使用DOTween的DOText

---
+++ candle-unity/Assets/Scripts/LearnDOTween/TestDoText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LearnDOTween
+{
+    public class TestDoText : MonoBehaviour
+    {
+        [SerializeField] private Text text;
+
+        private async void Start()
+        {
+            await TestDoTextFunc();
+        }
+
+        private async UniTask TestDoTextFunc()
+        {
+            string hello = "Hello World! Hello World! Hello World! Hello World! Hello World!";
+            await UniTask.Delay(TimeSpan.FromSeconds(2f));
+            text.DOText(hello, 5f);
+        }
+    }
+}
